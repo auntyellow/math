8a3664c3428f5b62f3469d0f322c4119b2192c70
auntyellow <webmaster@xqbase.com>
2021-10-10 16:42:52 +0800

    cancel is much faster than factor

---
+++ projective/brianchon-h.py
@@ -0,0 +1,53 @@
+from sympy import Eq, expand, simplify, solve, symbols
+from homogeneous import *
+
+def pair(conic, line):
+    x, y = symbols('x, y')
+    p = solve([conic, Eq(y, line)], (x, y))
+    return (simplify(p[0][0]), simplify(p[0][1])), (simplify(p[1][0]), simplify(p[1][1]))
+
+def tangent_line(P):
+    a, b, c, d, e, f = symbols('a, b, c, d, e, f')
+    return expand(a*P[0] + b*P[1] + d*P[2]), expand(b*P[0] + c*P[1] + e*P[2]), expand(d*P[0] + e*P[1] + f*P[2])
+
+def main():
+    a, b, c, d, e, f, g, h, k, x, y = symbols('a, b, c, d, e, f, g, h, k, x, y')
+    conic = Eq(a*x**2 + 2*b*x*y + c*y**2 + 2*d*x + 2*e*y + f, 0)
+    (F, A), (D, C), (B, E) = pair(conic, g*x), pair(conic, h*x), pair(conic, k)
+    print('x_A =', A[0])
+    print('x_B =', B[0])
+    print('x_C =', C[0])
+    print('x_D =', D[0])
+    print('x_E =', E[0])
+    print('x_F =', F[0])
+    A, B, C = to_homogeneous(A), to_homogeneous(B), to_homogeneous(C)
+    D, E, F = to_homogeneous(D), to_homogeneous(E), to_homogeneous(F)
+    print('A:', A)
+    print('B:', B)
+    print('C:', C)
+    print('D:', D)
+    print('E:', E)
+    print('F:', F)
+    aa, bb, cc = tangent_line(A), tangent_line(B), tangent_line(C)
+    dd, ee, ff = tangent_line(D), tangent_line(E), tangent_line(F)
+    print('aa:', aa)
+    print('bb:', bb)
+    print('cc:', cc)
+    print('dd:', dd)
+    print('ee:', ee)
+    print('ff:', ff)
+    ad, db, bf, fc, ce, ea = cross(aa, dd), cross(dd, bb), cross(bb, ff), cross(ff, cc), cross(cc, ee), cross(ee, aa)
+    print('ad:', ad)
+    print('db:', db)
+    print('bf:', bf)
+    print('fc:', fc)
+    print('ce:', ce)
+    print('ea:', ea)
+    gg, hh, jj = cross(ad, fc), cross(db, ce), cross(bf, ea)
+    print('gg:', gg)
+    print('hh:', hh)
+    print('jj:', jj)
+    print(incidence(gg, hh, jj))
+
+if __name__ == '__main__':
+    main()
--- projective/homogeneous.py
+++ projective/homogeneous.py
@@ -1,31 +1,28 @@
-from sympy import Matrix, factor, fraction, gcd_list, lcm_list
+from sympy import Matrix, cancel, fraction, gcd_list, lcm_list
 
 def reduced(x, y, z):
-    x1, y1, z1 = factor(x), factor(y), factor(z)
-    divisor = factor(gcd_list([x1, y1, z1]))
-    if divisor == 0:
+    gcd = gcd_list([x, y, z])
+    if gcd == 0:
         return 0, 0, 1
-    return x1/divisor, y1/divisor, z1/divisor
+    return cancel(x/gcd), cancel(y/gcd), cancel(z/gcd)
 
 def span(m, P1, n, P2):
     return reduced(m*P1[0] + n*P2[0], m*P1[1] + n*P2[1], m*P1[2] + n*P2[2])
 
-def cross(P1, P2, reduce = True):
+def cross(P1, P2):
     a, b, c, d, e, f = P1[0], P1[1], P1[2], P2[0], P2[1], P2[2]
     # | a b c |
     # | d e f |
     # | x y z |
-    if reduce:
-        return reduced(b*f - c*e, c*d - a*f, a*e - b*d)
-    return b*f - c*e, c*d - a*f, a*e - b*d
+    return reduced(b*f - c*e, c*d - a*f, a*e - b*d)
 
 def incidence(P1, P2, P3):
     return Matrix([[P1[0], P1[1], P1[2]], [P2[0], P2[1], P2[2]], [P3[0], P3[1], P3[2]]]).det()
 
 def multiplied(x, y, z):
-    x1, y1, z1 = fraction(factor(x)), fraction(factor(y)), fraction(factor(z))
+    x1, y1, z1 = fraction(cancel(x)), fraction(cancel(y)), fraction(cancel(z))
     lcd = lcm_list([x1[1], y1[1], z1[1]])
-    return x1[0]*(lcd/x1[1]), y1[0]*(lcd/y1[1]), z1[0]*(lcd/z1[1])
+    return x1[0]*cancel(lcd/x1[1]), y1[0]*cancel(lcd/y1[1]), z1[0]*cancel(lcd/z1[1])
 
 def to_homogeneous(P):
     return multiplied(P[0], P[1], 1)
