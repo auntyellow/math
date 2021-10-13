bc6069923a31005c53eef32375de3505103fc96c
auntyellow <webmaster@xqbase.com>
2021-10-13 14:07:04 +0800

    solved imo-202-shortlist-g7

---
+++ pythagoras/homogeneous.py
@@ -0,0 +1,28 @@
+from sympy import Matrix, cancel, fraction, gcd_list, lcm_list
+
+def reduced(x, y, z):
+    gcd = gcd_list([x, y, z])
+    if gcd == 0:
+        return 0, 0, 1
+    return cancel(x/gcd), cancel(y/gcd), cancel(z/gcd)
+
+def span(m, P1, n, P2):
+    return reduced(m*P1[0] + n*P2[0], m*P1[1] + n*P2[1], m*P1[2] + n*P2[2])
+
+def cross(P1, P2):
+    a, b, c, d, e, f = P1[0], P1[1], P1[2], P2[0], P2[1], P2[2]
+    # | a b c |
+    # | d e f |
+    # | x y z |
+    return reduced(b*f - c*e, c*d - a*f, a*e - b*d)
+
+def incidence(P1, P2, P3):
+    return Matrix([[P1[0], P1[1], P1[2]], [P2[0], P2[1], P2[2]], [P3[0], P3[1], P3[2]]]).det()
+
+def multiplied(x, y, z):
+    x1, y1, z1 = fraction(cancel(x)), fraction(cancel(y)), fraction(cancel(z))
+    lcd = lcm_list([x1[1], y1[1], z1[1]])
+    return x1[0]*cancel(lcd/x1[1]), y1[0]*cancel(lcd/y1[1]), z1[0]*cancel(lcd/z1[1])
+
+def to_homogeneous(P):
+    return multiplied(P[0], P[1], 1)
--- pythagoras/imo-2002-shortlist-g7.py
+++ pythagoras/imo-2002-shortlist-g7.py
@@ -1,9 +1,6 @@
-from sympy import Eq, Poly, solve, sqrt, symbols
+from sympy import Eq, expand, poly, solve, sqrt, symbols
 from cartesian import *
-
-def circle_eq(P):
-    x, y = P[0], P[1]
-    return Eq(x**2 + y**2 + d*x + e*y + f, 0)
+from homogeneous import *
 
 def circle(P1, P2, P3):
     # return F(x, y) such that F(x, y) = 0 is the circle's equation
@@ -16,34 +13,47 @@ def circle(P1, P2, P3):
     s = solve(circle_eqs, (d, e, f))
     return x**2 + y**2 + s[d]*x + s[e]*y + s[f]
 
+def center(c):
+    x, y = symbols('x, y')
+    return cancel(-poly(c, x).nth(1)/2), cancel(-poly(c, y).nth(1)/2)
+
 def main():
     # https://imomath.com/index.php?options=323 (Problem 14)
-    a, b, c = symbols('a, b, c', positive = True)
+    a, b, c, AB, AC = symbols('a, b, c, AB, AC', positive = True)
     A, B, C, M = (0, a), (-b, 0), (c, 0), (0, a/2)
     #AB, AC = sqrt(a**2 + b**2), sqrt(a**2 + c**2)
 
-    # Step 1: Calculate K and Incircle
-    AB, AC = symbols('AB, AC')
+    # Step 1: Solve K and incircle
     BC = b + c
     BK = (AB + BC - AC)/2
-    CK = (AB + AC - BC)/2
     K = (BK - b, 0)
-    print('K:', K)
+    CK = BC - BK
     K_B = (c - c*CK/AC, a*CK/AC)
     K_C = (b*BK/AB - b, a*BK/AB)
     incircle = circle(K, K_B, K_C)
+    I = center(incircle)
+    print('I =', I)
 
     # Step 2: Solve N
     KM = line(K, M)
-    print('KM:', KM)
     x, y = symbols('x, y')
     y_KM = solve(KM, y)[0]
-    print('y_KM =', y_KM)
-    incircle_poly = Poly(incircle.subs(y, y_KM), x)
+    incircle_poly = poly(incircle.subs(y, y_KM), x)
     incircle_coeffs = incircle_poly.all_coeffs()
+    # Vieta's Formula: x_N + x_K = -b/a
     x_N = -incircle_coeffs[1]/incircle_coeffs[0] - K[0]
-    N = (x_N, y_KM.subs(x, x_N))
-    print('N:', N)
+    N = (cancel(x_N), cancel(y_KM.subs(x, x_N)))
+    print('N =', N)
+
+    # Step 3: Solve BCN and center S
+    x_N, y_N = symbols('x_N, y_N')
+    S = center(circle((x_N, y_N), B, C))
+    S = (cancel(S[0].subs(x_N, N[0]).subs(y_N, N[1])), cancel(S[1].subs(x_N, N[0]).subs(y_N, N[1])))
+    print('S =', S)
+
+    # Step 4: Prove INS collinear
+    collinear = incidence(to_homogeneous(I), to_homogeneous(N), to_homogeneous(S))
+    print(expand(collinear.subs(AB, sqrt(a**2 + b**2)).subs(AC, sqrt(a**2 + c**2))))
 
 if __name__ == '__main__':
     main()
