4c2ec565c48c2cef025c4d921858ec402b516de0
auntyellow <webmaster@xqbase.com>
2021-10-14 14:01:55 +0800

    another root should be given by Vieta's formula

--- pythagoras/duke-putnam-2012-homework-6-7.py
+++ pythagoras/duke-putnam-2012-homework-6-7.py
@@ -1,32 +1,24 @@
-from sympy import *
+from sympy import Eq, simplify, solve, sqrt, symbols
+from cartesian import *
 
-def intersect(L1, L2):
-    p = solve([L1, L2], (x, y))
-    return simplify(p[x]), simplify(p[y])
+def main():
+    # https://www.imomath.com/index.php?options=586 (Problem 7)
+    a, b, r, AC = symbols('a, b, r, AC', positive = True)
+    c, x, y = symbols('c, x, y')
+    A, B, C, O = (-a, 0), (0, 0), (b, c), (0, r)
+    AC0 = sqrt((a + b)**2 + c**2)
+    P = (a*(a + b)/AC - a, a*c/AC)
+    r = solve(Eq((P[0] - O[0])**2 + (P[1] - O[1])**2, r**2), r)[0]
+    print('r =', r)
+    r = a*((a + b)**2 + c**2 - 2*AC*a - 2*AC*b + a**2 + 2*a*b + b**2 + c**2)/(2*AC*c)
+    BC, BP = line(B, C), line(B, P)
+    OH = Eq(y, r - b*x/c)
+    T = intersect(BP, OH)
+    print('T:', T)
+    M = intersect(BC, line(A, T))
+    print('M:', M)
+    print('M = (B + C)/2 in x?', simplify(simplify(B[0] + C[0] - 2*M[0]).subs(AC, AC0)) == 0)
+    print('M = (B + C)/2 in y?', simplify(simplify(B[1] + C[1] - 2*M[1]).subs(AC, AC0)) == 0)
 
-def line(p1, p2):
-    x1, y1, x2, y2, x3, y3 = x, y, p1[0], p1[1], p2[0], p2[1]
-    return Eq(simplify(x1 * y2 + x2 * y3 + x3 * y1 - x2 * y1 - x3 * y2 - x1 * y3), 0)
-
-# https://www.imomath.com/index.php?options=586 (Problem 7)
-a, b, r, AC = symbols('a, b, r, AC', positive = True)
-c, x, y = symbols('c, x, y')
-A, B, C, O = (-a, 0), (0, 0), (b, c), (0, r)
-P = (a * (a + b) / AC - a, a * c / AC)
-r = solve(Eq((P[0] - O[0])**2 + (P[1] - O[1])**2, r**2), r)[0]
-print('r =', r)
-
-# AC^2=(a+b)^2+c^2
-r = a*((a + b)**2 + c**2 - 2*AC*a - 2*AC*b + a**2 + 2*a*b + b**2 + c**2)/(2*AC*c)
-BC, BP = line(B, C), line(B, P)
-OH = Eq(y, r - b * x / c)
-T = intersect(BP, OH)
-print('T:', T)
-M = intersect(BC, line(A, T))
-print('M:', M)
-print(simplify(B[0] + C[0] - 2 * M[0]))
-# b*(AC**2 - a**2 - 2*a*b - b**2 - c**2)/(AC**2 - 2*AC*a - 2*AC*b + a**2 + 2*a*b + b**2 + c**2)
-print(simplify(B[1] + C[1] - 2 * M[1]))
-# c*(AC**2 - a**2 - 2*a*b - b**2 - c**2)/(AC**2 - 2*AC*a - 2*AC*b + a**2 + 2*a*b + b**2 + c**2)
-
-# AC**2 - a**2 - 2*a*b - b**2 - c**2 = (a + b)**2 + c**2 - a**2 - 2*a*b - b**2 = 0
+if __name__ == '__main__':
+    main()
--- pythagoras/imo-2003-shortlist-g4.py
+++ pythagoras/imo-2003-shortlist-g4.py
@@ -1,12 +1,19 @@
-from sympy import Eq, simplify, solve, symbols
+from sympy import Eq, poly, simplify, solve, symbols
 
 def circle(G):
     x, y = symbols('x, y')
     return Eq((x - G[0])**2 + (y - G[1])**2, G[0]**2 + G[1]**2)
 
 def intersect(Ga, Gb):
+    x, y = symbols('x, y')
     # root[0] is (0, 0) so we choose root[1]
-    return solve([Ga, Gb], symbols('x, y'))[1]
+    # return solve([Ga, Gb], (x, y))[1]
+    # We cannot guarantee (0, 0) is root[0] so another root should be given by Vieta's formula
+    yi = solve(Eq(Ga.lhs - Gb.lhs, Ga.rhs - Gb.rhs), y)[0]
+    eqGa = Ga.subs(y, yi)
+    coeffs = poly(eqGa.lhs - eqGa.rhs, x).all_coeffs()
+    xi = -coeffs[1]/coeffs[0]
+    return xi, yi.subs(x, xi)
 
 def dist2(P1, P2):
     return (P1[0] - P2[0])**2 + (P1[1] - P2[1])**2
@@ -17,6 +24,12 @@ def main():
     G1, G2, G3, G4 = circle((a, 0)), circle((b, k*b)), circle((c, 0)), circle((d, k*d))
     A, B, C, D, P = intersect(G1, G2), intersect(G2, G3), intersect(G3, G4), intersect(G4, G1), (0, 0)
     AB, BC, CD, DA, PB, PD = dist2(A, B), dist2(B, C), dist2(C, D), dist2(D, A), dist2(P, B), dist2(P, D)
+    print('AB =', AB)
+    print('BC =', BC)
+    print('CD =', CD)
+    print('DA =', DA)
+    print('PB =', PB)
+    print('PD =', PD)
     print('AB*BC*PD**2 - CD*DA*PB**2 =', simplify(AB*BC*PD**2 - CD*DA*PB**2))
 
 if __name__ == '__main__':
