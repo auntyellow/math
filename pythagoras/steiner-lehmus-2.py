53a2e69caaec0bdf16874954fc7d334a3b972a06
auntyellow <webmaster@xqbase.com>
2021-11-19 15:01:26 +0800

    Adjust letter to match original problem
    https://math.stackexchange.com/questions/2391840

--- pythagoras/steiner-lehmus-2.py
+++ pythagoras/steiner-lehmus-2.py
@@ -6,25 +6,25 @@ def dist2(P1, P2):
 
 def main():
     # https://math.stackexchange.com/questions/2391840
-    # 0 < ∠CAB = 2α < π/2, 0 < ∠CBA = 2β < π/2
-    # ∠DAB = α + δ, ∠DAC = α - δ, ∠EBA = β + δ, ∠EBC = β - δ, |δ| < min(α,β)
-    # ∠DAB + ∠EBC = ∠DAC + ∠EBA = α + β, AD = AE, prove α = β
-    # 0 < a = tan α < 1, 0 < b = tan β < 1, d = tan δ, |d| < min(a,b)
+    # 0 < ∠ABC = 2β < π/2, 0 < ∠ACB = 2γ < π/2
+    # ∠DBC = β + δ, ∠DBA = β - δ, ∠ECB = γ + δ, ∠EBA = γ - δ, |δ| < min(β,γ)
+    # ∠DBC + ∠ECA = ∠DBA + ∠ECB = β + γ, BD = CE, prove β = γ
+    # 0 < b = tan β < 1, 0 < c = tan γ < 1, d = tan δ, |d| < min(b,c)
     # If δ = 0, it's Steiner-Lehmus theorem 
     a, b, c = symbols('a, b, c', positive = True)
     d, x, y = symbols('d, x, y')
-    tan_2a = 2*a/(1 - a**2)
     tan_2b = 2*b/(1 - b**2)
-    tan_ad = (a + d)/(1 - a*d)
-    tan_bd = (b + d)/(1 - b*d) 
-    A, B, C = (-c/tan_2a, 0), (c/tan_2b, 0), (0, c)
-    AC, BC, AD, BE = line(A, C), line(B, C), Eq(y, tan_ad*(x - A[0])), Eq(y, -tan_bd*(x - B[0]))
-    D, E = intersect(AD, BC), intersect(BE, AC)
+    tan_2c = 2*c/(1 - c**2)
+    tan_bd = (b + d)/(1 - b*d)
+    tan_cd = (c + d)/(1 - c*d) 
+    A, B, C = (0, a), (-a/tan_2b, 0), (a/tan_2c, 0)
+    AB, AC, BD, CE = line(A, B), line(A, C), Eq(y, tan_bd*(x - B[0])), Eq(y, -tan_cd*(x - C[0]))
+    D, E = intersect(BD, AC), intersect(CE, AB)
     print('D:', D)
     print('E:', E)
-    print('AD**2 - BE**2 =', factor(dist2(A, D) - dist2(B, E)))
+    print('BD**2 - CE**2 =', factor(dist2(B, D) - dist2(C, E)))
     # should prove this factor is always negative:
-    p = a**5*b**4 - 2*a**5*b**3*d + a**5*b**2*d**2 + a**4*b**5 - 6*a**4*b**4*d + 5*a**4*b**3*d**2 - 4*a**4*b**3 + 4*a**4*b**2*d - 2*a**3*b**5*d + 5*a**3*b**4*d**2 - 4*a**3*b**4 + 14*a**3*b**3*d - 5*a**3*b**2*d**2 + 5*a**3*b**2 - 4*a**3*b*d - a**3 + a**2*b**5*d**2 + 4*a**2*b**4*d - 5*a**2*b**3*d**2 + 5*a**2*b**3 - 14*a**2*b**2*d + 4*a**2*b*d**2 - 5*a**2*b + 2*a**2*d - 4*a*b**3*d + 4*a*b**2*d**2 - 5*a*b**2 + 6*a*b*d - a*d**2 - b**3 + 2*b**2*d - b*d**2
+    p = b**5*c**4 + 2*b**5*c**3*d + b**5*c**2*d**2 + b**4*c**5 + 6*b**4*c**4*d + 5*b**4*c**3*d**2 - 4*b**4*c**3 - 4*b**4*c**2*d + 2*b**3*c**5*d + 5*b**3*c**4*d**2 - 4*b**3*c**4 - 14*b**3*c**3*d - 5*b**3*c**2*d**2 + 5*b**3*c**2 + 4*b**3*c*d - b**3 + b**2*c**5*d**2 - 4*b**2*c**4*d - 5*b**2*c**3*d**2 + 5*b**2*c**3 + 14*b**2*c**2*d + 4*b**2*c*d**2 - 5*b**2*c - 2*b**2*d + 4*b*c**3*d + 4*b*c**2*d**2 - 5*b*c**2 - 6*b*c*d - b*d**2 - c**3 - 2*c**2*d - c*d**2
 
 if __name__ == '__main__':
     main()
