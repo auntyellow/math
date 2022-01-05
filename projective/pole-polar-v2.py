6fad675dd1570ba1766ba97821f5992c804ef4e6
auntyellow <webmaster@xqbase.com>
2022-01-05 08:53:39 +0800

    pole-polar

--- projective/pole-polar-v2.py
+++ projective/pole-polar-v2.py
@@ -8,9 +8,9 @@ def lies_on(P, L):
     return expand(P[0]*L[0] + P[1]*L[1] + P[2]*L[2]) == 0
 
 def main():
-    a, b, c, d, e, s, t, u, v, w = symbols('a, b, c, d, e, s, t, u, v, w')
+    a, b, t, u, v, w, x, y, z = symbols('a, b, c, d, e, t, x, y, z')
     # results from steiner-conic-v.py
-    P, M, N = (c, d, e), (t*(a + t), t*(b + t), (a + t)*(b + t)), (u*(a + u), u*(b + u), (a + u)*(b + u))
+    P, M, N = (x, y, z), (t*(a + t), t*(b + t), (a + t)*(b + t)), (u*(a + u), u*(b + u), (a + u)*(b + u))
     u = solve(Eq(incidence(P, M, N), 0), u)
     u = u[1] if u[0] == t else u[0]
     N = multiplied(u*(a + u), u*(b + u), (a + u)*(b + u))
@@ -21,14 +21,11 @@ def main():
     print('Q:', Q)
     polarP = cross(Q, point_subs(Q, t, w))
     print('P\'s Polar:', polarP)
-    R = span(s, point_subs(Q, t, 0), 1, point_subs(Q, t, 1))
-    print('R on P\'s Polar:', R)
-    print('Is R on P\'s Polar?', lies_on(R, polarP))
-    cc, dd, ee = symbols('cc, dd, ee')
-    subs = [(c, cc), (d, dd), (e, ee), (cc, R[0]), (dd, R[1]), (ee, R[2])]
-    polarR = reduced(polarP[0].subs(subs), polarP[1].subs(subs), polarP[2].subs(subs))
-    print('R\'s Polar:', polarR)
-    print('Is P on R\'s Polar?', lies_on(P, polarR))
+    xx, yy, zz = symbols('xx, yy, zz')
+    subs = [(x, xx), (y, yy), (z, zz), (xx, Q[0]), (yy, Q[1]), (zz, Q[2])]
+    polarQ = reduced(polarP[0].subs(subs), polarP[1].subs(subs), polarP[2].subs(subs))
+    print('Q\'s Polar:', polarQ)
+    print('Is P on Q\'s Polar?', lies_on(P, polarQ))
 
 if __name__ == '__main__':
     main()
