3fea9ca92d9f0576c9b7f0e0c920a3ace517a181
auntyellow <webmaster@xqbase.com>
2023-09-08 16:26:57 +0800

    4765317

--- inequality/4692575.py
+++ inequality/4692575.py
@@ -1,8 +1,7 @@
 from sympy import *
 
-# https://math.stackexchange.com/q/4692575
-
 def main():
+    # https://math.stackexchange.com/q/4692575
     a, b, c = symbols('a, b, c', positive = True)
     f = 1/a + 1/b + 1/c + 6/(a + b + c) - 5
     u, v = symbols('u, v', positive = True)
@@ -10,6 +9,17 @@ def main():
     print('f(a1bc) =', factor(f.subs(a, 1/b/c).subs(b, 1 + u).subs(c, 1 + v)))
     # a, b <= 1 <= c
     print('f(ab1c) =', factor(f.subs(c, 1/a/b).subs(a, 1/(1 + u)).subs(b, 1/(1 + v))))
+    print()
+
+    # https://math.stackexchange.com/q/4765317
+    f = 1/a + 1/b + 1/c - 6/(a + b + c) - 1
+    w = symbols('w', positive = True)
+    # a <= 1 <= b, c
+    print('f(a1bc) =', factor(f.subs(a, 1/(b*c + w)).subs(b, 1 + u).subs(c, 1 + v)))
+    # a, b <= 1 <= c <= 1/ab
+    print('f(ab1c) =', factor(f.subs(c, 1 + (1/a/b - 1)/(1 + w)).subs(a, 1/(1 + u)).subs(b, 1/(1 + v))))
+    # a, b, c <= 1
+    print('f(abc1) =', factor(f.subs(a, 1/(1 + u + v + w)).subs(b, 1/(1 + u + v)).subs(c, 1/(1 + u))))
 
 if __name__ == '__main__':
     main()
