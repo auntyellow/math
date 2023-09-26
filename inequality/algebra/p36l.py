fbb7f994b0a802857112923c2248967c13e34db5
auntyellow <webmaster@xqbase.com>
2023-09-26 22:34:29 +0800

    solved p36, §2.1, ex3 by Lagrange multiplier

--- inequality/algebra/p36l.py
+++ inequality/algebra/p36l.py
@@ -3,9 +3,20 @@ from sympy import *
 # ISBN 9787542848482, p36, §2.1, ex3
 # x**2 + y**2 + z**2 + x*y*z = 4 -> 0 <= x*y + y*z + z*x - x*y*z <= 2
 
-def main():
+def dump(s, f):
+    x0, y0, z0, l0 = s
+    if x0 < 0 or y0 < 0 or z0 < 0:
+        return
+    x0, y0, z0 = simplify(x0), simplify(y0), simplify(z0)
+    print('x =', x0)
+    print('y =', y0)
+    print('z =', z0)
     x, y, z = symbols('x, y, z')
-    l = symbols('l')
+    print('f_m =', N(f.subs(x, x0).subs(y, y0).subs(z, z0)))
+    print()
+
+def main():
+    x, y, z, l = symbols('x, y, z, l')
     g = x**2 + y**2 + z**2 + x*y*z - 4
     f = x*y + y*z + z*x - x*y*z
     L = f + l*g
@@ -13,63 +24,18 @@ def main():
     print('L_x =', Lx)
     print('L_y =', Ly)
     print('L_z =', Lz)
-    # Unable to solve by SymPy
-    # s = solve([Eq(Lx, 0), Eq(Ly, 0), Eq(Lz, 0), Eq(g, 0)], (x, y, z, l))
-
-    l0 = solve(Eq(Lx, 0), l)
-    print('l =', l0)
-    l0 = l0[0]
-    x0 = solve(Eq(Ly.subs(l, l0), 0), x)
-    print('x =', x0)
-    # skip x = y
-    x0 = x0[1]
-    y0 = solve(Eq(Lz.subs(l, l0), 0).subs(x, x0), y)
-    print('y =', y0)
-    # skip y = z
-    y0 = y0[1]
-    z0 = solve(Eq(g.subs(x, x0).subs(y, y0), 0), z)
-    print('z =', z0)
-    # only root "1/4 + sqrt(17)/4" makes y and x non-negative
-    z0 = z0[3]
-    y0 = y0.subs(z, z0)
-    print('y =', y0)
-    x0 = x0.subs(y, y0).subs(z, z0)
-    print('x =', x0)
-    f0 = f.subs(x, x0).subs(y, y0).subs(z, z0)
-    print('f_m =', N(f0))
     print()
+    ss = solve([Eq(Lx, 0), Eq(Ly, 0), Eq(Lz, 0), Eq(g, 0)], (x, y, z, l))
+    for s in ss:
+        dump(s, f)
 
-    # now try x = y
-    x0 = y
-    y0 = solve(Eq(Lz.subs(l, l0), 0).subs(x, x0), y)
-    print('y =', y0)
-    # skip y = z
-    y0 = y0[2]
-    z0 = solve(Eq(g.subs(x, x0).subs(y, y0), 0), z)
-    print('z =', z0)
-    z0 = z0[1]
-    # only root "7/8 - sqrt(17)/8" makes y and x non-negative
-    y0 = y0.subs(z, z0)
-    print('y =', y0)
-    x0 = x0.subs(y, y0).subs(z, z0)
-    print('x =', x0)
-    f0 = f.subs(x, x0).subs(y, y0).subs(z, z0)
-    print('f_m =', N(f0))
-    print()
+    ss = solve([Eq(x, 0), Eq(Ly, 0), Eq(Lz, 0), Eq(g, 0)], (x, y, z, l))
+    for s in ss:
+        dump(s, f)
 
-    # now try x = y = z
-    x0 = z
-    y0 = z
-    z0 = solve(Eq(g.subs(x, x0).subs(y, y0), 0), z)
-    print('z =', z0)
-    z0 = z0[1]
-    # only root "1" makes y and x non-negative
-    y0 = y0.subs(z, z0)
-    print('y =', y0)
-    x0 = x0.subs(y, y0).subs(z, z0)
-    print('x =', x0)
-    f0 = f.subs(x, x0).subs(y, y0).subs(z, z0)
-    print('f_m =', f0)
+    ss = solve([Eq(x, 0), Eq(y, 0), Eq(Lz, 0), Eq(g, 0)], (x, y, z, l))
+    for s in ss:
+        dump(s, f)
 
 if __name__ == '__main__':
     main()
