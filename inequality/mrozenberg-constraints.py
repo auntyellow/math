6dde64acc097715e14df2fe057321c97dd3eed9a
auntyellow <webmaster@xqbase.com>
2023-10-07 15:11:45 +0800

    MRozenbergInequalityWithConstraints done

--- inequality/mrozenberg-constraints.py
+++ inequality/mrozenberg-constraints.py
@@ -13,7 +13,27 @@ def sum_cyc(f, vars):
 
 def main():
     a, b, c = symbols('a, b, c', positive = True)
-    f = sum_cyc(4*a*b*(a**2 + b**2) - a**4 - 5*a**2*b**2 - 2*a**2*b*c)
+    f = sum_cyc(4*a*b*(a**2 + b**2) - a**4 - 5*a**2*b**2 - 2*a**2*b*c, (a, b, c))
+    print('f =', factor(f))
+    # see ggb, sufficient to prove:
+    # 1. a, b <= 1 <= c <= 2 (red) and
+    # 2. a <= 1 <= b, c <= 2 (organge)
+    x, y, z = symbols('x, y, z', positive = True)
+    xyz = x + y + z
+    a0, b0, c0 = 1 - x/xyz, 1 - y/xyz, 2 - z/xyz
+    print('a + b + c =', factor(a0 + b0 + c0))
+    f1 = f.subs(a, a0).subs(b, b0).subs(c, c0)
+    print('f1 =', factor(f1))
+    a0, b0, c0 = x/xyz, 1 + y/xyz, 1 + z/xyz
+    print('a + b + c =', factor(a0 + b0 + c0))
+    f2 = f.subs(a, a0).subs(b, b0).subs(c, c0)
+    print('f2 =', factor(f2))
+    # 3. a, b <= 1, 2 <= c: doesn't hold
+    a0, b0, c0 = x/xyz, y/xyz, 2 + z/xyz
+    print('a + b + c =', factor(a0 + b0 + c0))
+    f3 = f.subs(a, a0).subs(b, b0).subs(c, c0)
+    print('f3 =', factor(f3))
+    print('f(1/3,1/3,7/3) =', f.subs(a, S(1)/3).subs(b, S(1)/3).subs(c, S(7)/3))
 
 if __name__ == '__main__':
     main()
