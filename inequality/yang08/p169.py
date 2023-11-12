b2112f50a202656b6efc23af0220d134a9222fe2
auntyellow <webmaster@xqbase.com>
2023-11-12 23:47:59 +0800

    Java Prover: ignore output

--- inequality/yang08/p169.py
+++ inequality/yang08/p169.py
@@ -24,6 +24,8 @@ def main():
     f = sum_comb4((-x3**2 - 2*x4*x1 + 6*x1**2 + 6*x2**2 + 4*x2*x1 - x4**2 - 2*x2*x3 - 2*x3*x1 - 2*x4*x2)*(x1 - x2)**n, (x1, x2, x3, x4))
     a, u, v, w = symbols('a, u, v, w', positive = True)
     print('f =', expand(f.subs(x1, a).subs(x2, a + u).subs(x3, a + u + v).subs(x4, a + u + v + w)))
+    # same speed, same result
+    # print('f =', expand(f.subs(x4, x3 + w).subs(x3, x2 + v).subs(x2, x1 + u).subs(x1, a)))
 
 if __name__ == '__main__':
     main()
