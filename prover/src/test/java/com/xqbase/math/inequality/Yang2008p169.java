4758f5345529d958948374d172f2497e2a455863
auntyellow <webmaster@xqbase.com>
2023-11-14 22:44:37 +0800

    Java Prover: improvement, n = 200

--- prover/src/test/java/com/xqbase/math/inequality/Yang2008p169.java
+++ prover/src/test/java/com/xqbase/math/inequality/Yang2008p169.java
@@ -3,12 +3,12 @@ package com.xqbase.math.inequality;
 import java.io.PrintStream;
 import java.math.BigInteger;
 import java.util.Map;
+import java.util.TreeMap;
 
 import com.xqbase.math.polys.BigMono;
 import com.xqbase.math.polys.BigPoly;
 
 public class Yang2008p169 {
-	private static final int MAX_EXP = 53;
 	private static final BigInteger _1 = BigInteger.ONE;
 	private static final String VARS = "abcduvw";
 	private static final String SUB_POLY = "-x3**2 - 2*x4*x1 + 6*x1**2 + 6*x2**2 + 4*x2*x1 - x4**2 - 2*x2*x3 - 2*x3*x1 - 2*x4*x2";
@@ -29,26 +29,31 @@ public class Yang2008p169 {
 		return p;
 	}
 
-	public static BigPoly subs(BigPoly p0, String from, String to) {
-		BigPoly p = new BigPoly();
-		BigPoly toP = new BigPoly(VARS, to);
-		BigPoly[] pows = new BigPoly[MAX_EXP];
-		BigPoly t = new BigPoly(VARS, "1");
-		pows[0] = t;
-		for (int i = 1; i < MAX_EXP; i ++) {
-			t = new BigPoly().addMul(BigInteger.ONE, t, toP);
-			pows[i] = t;
-		}
-		char c = from.charAt(0);
-		for (Map.Entry<BigMono, BigPoly> entry : p0.coeffsOf(from).entrySet()) {
-			// p.addMul(_1, entry.getValue(), pow(toP, entry.getKey().getExps()[VARS.indexOf(c)]));
-			p.addMul(_1, entry.getValue(), pows[entry.getKey().getExps()[VARS.indexOf(c)]]);
+	public static BigPoly subs(BigPoly p0, char from, String to) {
+		BigPoly x = new BigPoly(VARS, to);
+		TreeMap<BigMono, BigPoly> ai = p0.coeffsOf(String.valueOf(from));
+		// a_0x^n+a_1x^{n-1}+a_2x^{n-2}+...+a_{n-1}x+a_n = (...((a_0x+a_1)x+a2)+...+a_{n-1})x+a_n
+		Map.Entry<BigMono, BigPoly> lt = ai.firstEntry();
+		System.out.println(lt);
+		BigPoly p = lt.getValue();
+		short[] exps = new short[VARS.length()];
+		int fromIndex = VARS.indexOf(from);
+		for (int i = lt.getKey().getExps()[fromIndex] - 1; i >= 0; i --) {
+			exps[fromIndex] = (short) i;
+			BigPoly p1 = new BigPoly();
+			p1.addMul(_1, p, x);
+			BigPoly a = ai.get(new BigMono(VARS, exps));
+			if (a != null) {
+				p1.add(_1, a);
+			}
+			p = p1;
+			System.out.println(i + "," + p.size());
 		}
 		return p;
 	}
 
 	public static void main(String[] args) throws Exception {
-		int n = MAX_EXP - 3;
+		int n = 200;
 		BigPoly p = new BigPoly();
 		long t0 = System.currentTimeMillis();
 		p.addMul(_1, pow(new BigPoly(VARS, "a - b"), n), new BigPoly(VARS, sym("abcd")));
@@ -57,9 +62,15 @@ public class Yang2008p169 {
 		p.addMul(_1, pow(new BigPoly(VARS, "b - c"), n), new BigPoly(VARS, sym("bcad")));
 		p.addMul(_1, pow(new BigPoly(VARS, "b - d"), n), new BigPoly(VARS, sym("bdac")));
 		p.addMul(_1, pow(new BigPoly(VARS, "c - d"), n), new BigPoly(VARS, sym("cdab")));
-		p = subs(p, "b", "a + u");
-		p = subs(p, "c", "a + u + v");
-		p = subs(p, "d", "a + u + v + w");
+		/*
+		p = subs(p, 'b', "a + u");
+		p = subs(p, 'c', "a + u + v");
+		p = subs(p, 'd', "a + u + v + w");
+		*/
+		// much faster
+		p = subs(p, 'd', "c + w");
+		p = subs(p, 'c', "b + v");
+		p = subs(p, 'b', "a + u");
 		long t1 = System.currentTimeMillis();
 		System.out.println(t1 - t0);
 		try (PrintStream out = new PrintStream("Yang2008p169.py")) {
