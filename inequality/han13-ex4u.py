c5d23849000476831c01ad77a4c83fac9516939b
auntyellow <webmaster@xqbase.com>
2023-12-15 08:50:30 +0800

    try han13 ex5

--- inequality/han13-ex4u.py
+++ inequality/han13-ex4u.py
@@ -5,14 +5,16 @@ from sympy import *
 def main():
     a, b, c, u, v = symbols('a, b, c, u, v', negative = False)
     f = (a**2 + b**2 + c**2)**2 - 3*(a**3*b + b**3*c + c**3*a)
-    print('f(xyz) =', factor(f.subs(c, a*(1 + u + v)).subs(b, a*(1 + u))))
-    print('f(xzy) =', factor(f.subs(b, a*(1 + u + v)).subs(c, a*(1 + u))))
-    # f(xzy)
+    print('f(abc) =', factor(f.subs(c, a*(1 + u + v)).subs(b, a*(1 + u))))
+    print('f(acb) =', factor(f.subs(b, a*(1 + u + v)).subs(c, a*(1 + u))))
+    # f(acb)
     g = u**4 - u**3*v + u**3 - u**2*v**2 - 3*u**2*v + u**2 + u*v**3 - 2*u*v**2 + u*v + v**4 + v**3 + v**2
     # proved by polynomial-prover
     print('g(uv) =', factor(g.subs(v, u*(1 + v))))
     # unable to find rational zeros
     print('g(vu) =', factor(g.subs(u, v*(1 + u))))
+    # equality occurs when a = sin(pi/7)**2, b = sin(4*pi/7)**2 and c = sin(2*pi/7)**2
+    # see https://math.stackexchange.com/q/2587996/
 
 if __name__ == '__main__':
     main()
