06cf9f9b03e5dd882ae196301425d8f414a27112
auntyellow <webmaster@xqbase.com>
2023-12-29 09:27:14 +0800

    try han23-p341

---
+++ inequality/algebra/han23-p341u.py
@@ -0,0 +1,11 @@
+from sympy import *
+
+# ISBN 9787312056185, p341, ex 11.7
+
+def main():
+    a, b, c, u, v, w = symbols('a, b, c, u, v, w', negative = False)
+    f = 1/(a + u*b)/(a + v*b) + 1/(b + u*c)/(b + v*c) + 1/(c + u*a)/(c + v*a) - 9/(1 + u)/(1 + v)/(a*b + b*c + c*a)
+    print('fn =', Poly(fraction(cancel(f))[0]).homogenize(w).expr)
+
+if __name__ == '__main__':
+    main()
--- inequality/radical/han14-p301.py
+++ inequality/radical/han14-p301.py
@@ -1,14 +1,5 @@
 from sympy import *
 
-def cyc(f, vars):
-    x, y, z = vars
-    t = symbols('t', negative = False)
-    return f.subs(z, t).subs(y, z).subs(x, y).subs(t, x)
-
-def sum_cyc(f, vars):
-    f1 = cyc(f, vars)
-    return f + f1 + cyc(f1, vars)
-
 def main():
     a, b, c = symbols('a, b, c', negative = False)
     # use conclusion from radical3.py
