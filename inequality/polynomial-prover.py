c370f606258a4e15092173cafe48b5e93bcab004
auntyellow <webmaster@xqbase.com>
2024-01-23 10:24:02 +0800

    optimize polynomial-prover
    TODO: critical point of 1777075?

--- inequality/polynomial-prover.py
+++ inequality/polynomial-prover.py
@@ -3,7 +3,7 @@ from sympy import *
 
 def sign(f):
     # return 0 if sign is not determined
-    u, v, w = symbols('u, v, w', negative = False)
+    u, v = symbols('u, v', negative = False)
     if f.func == Pow:
         return sign(f.args[0])**f.args[1]
     if f.func == Mul:
@@ -14,7 +14,7 @@ def sign(f):
                 return 0
             s *= s0
         return s
-    p = Poly(f, u, v, w)
+    p = Poly(f, u, v)
     pos = false
     neg = false
     for coef in p.coeffs():
@@ -28,98 +28,90 @@ def sign(f):
             neg = true
     return 1 if pos else (-1 if neg else 0)
 
+warning_set = set()
+warning_set2 = set()
+
+def warn(f, f0, x0, y0):
+    tuple = (f, f0, x0, y0)
+    if not tuple in warning_set:
+        warning_set.add(tuple)
+        logging.warning('{} = {} at ({}, {})'.format(f, f0, x0, y0))
+
 # May not work if:
 # 1. zero on x or y != m/2^n, e.g. (x-1/3)^2+y^2
 # 2. critical point near x = y or x = 1/y (why?)
 #    example: g in 4746804.py, so try x <= y and y <= x (why usually work?)
 
-def negative(f, x0 = 0, x1 = oo, y0 = 0, y1 = oo):
+# return None if f >= 0, or the counterexample (x0, y0, f0)
+# param f: 2-var function about x and y
+def negative(f, x0, x1, y0, y1):
     x, y = symbols('x, y', negative = False)
 
     # try to find counterexample
-    f0 = f.subs(x, x0).subs(y, y0)
+    f0, f1 = f.subs(x, x1).subs(y, y1), f.subs(y, y1).subs(x, x1)
+    if f0 == nan or abs(f0) == oo:
+        warn(f, f0, x1, y1)
+        f0 = 0
+    if f1 == nan or abs(f1) == oo:
+        warn(f, f1, x1, y1)
+        f1 = 0
+    if f0 != f1:
+        tuple = (f, f0, f1, x1, y1)
+        if not tuple in warning_set2:
+            warning_set2.add(tuple)
+            logging.warning('{} = {} or {} at ({}, {})'.format(f, f0, f1, x1, y1))
+    f0 = min(f0, f1)
     if f0 < 0:
-        return 'f({},{})={}'.format(x0, y0, f0)
+        return x1, y1, f0
 
     # try to prove by buffalo way
     dx, dy = x1 - x0, y1 - y0
     u, v = symbols('u, v', negative = False)
-    f1 = f.subs(x, x0 + (u if dx == oo else dx/(1 + u))). \
-        subs(y, y0 + (v if dy == oo else dy/(1 + v)))
+    f1 = f.subs(x, x0 + dx/(1 + u)).subs(y, y0 + dy/(1 + v))
     f1 = factor(f1)
     s = sign(f1)
     if s > 0:
         logging.info('non_negative: [{},{},{},{}], f={}'.format(x0, x1, y0, y1, f0))
-        return ''
+        return None
     elif s < 0:
-        return 'f={}<0'.format(f1)
+        logging.warn('should be negative: f={}<0'.format(f1))
 
     logging.info('try_dividing: [{},{},{},{}], f={}'.format(x0, x1, y0, y1, f0))
 
     # divide
-    xm = (1 if x0 == 0 else x0*2) if x1 == oo else x0 + dx/S(2)
-    ym = (1 if y0 == 0 else y0*2) if y1 == oo else y0 + dy/S(2)
-    n = negative(f, xm, x1, ym, y1)
-    if n != '':
+    if dx < dy:
+        ym = y0 + dy/S(2)
+        n = negative(f, x0, x1, y0, ym)
+        if n != None:
+            return n
+        return negative(f, x0, x1, ym, y1)
+    xm = x0 + dx/S(2)
+    n = negative(f, x0, xm, y0, y1)
+    if n != None:
         return n
-    n = negative(f, x0, xm, ym, y1)
-    if n != '':
-        return n
-    n = negative(f, xm, x1, y0, ym)
-    if n != '':
-        return n
-    return negative(f, x0, xm, y0, ym)
+    return negative(f, xm, x1, y0, y1)
 
-def negative3(f, x0 = 0, x1 = oo, y0 = 0, y1 = oo, z0 = 0, z1 = oo):
-    x, y, z = symbols('x, y, z', negative = False)
-
-    # try to find counterexample
-    f0 = f.subs(x, x0).subs(y, y0).subs(z, z0)
-    if f0 < 0:
-        return 'f({},{},{})={}'.format(x0, y0, z0, f0)
-
-    # try to prove by buffalo way
-    dx, dy, dz = x1 - x0, y1 - y0, z1 - z0
-    u, v, w = symbols('u, v, w', negative = False)
-    f1 = f.subs(x, x0 + (u if dx == oo else dx/(1 + u))). \
-        subs(y, y0 + (v if dy == oo else dy/(1 + v))). \
-        subs(z, z0 + (w if dz == oo else dz/(1 + w)))
-    f1 = factor(f1)
-    s = sign(f1)
-    if s > 0:
-        logging.info('non_negative: [{},{},{},{},{},{}], f={}'.format(x0, x1, y0, y1, z0, z1, f0))
-        return ''
-    elif s < 0:
-        return 'f={}<0'.format(f1)
-
-    logging.info('try_dividing: [{},{},{},{},{},{}], f={}'.format(x0, x1, y0, y1, z0, z1, f0))
-
-    # divide
-    xm = (1 if x0 == 0 else x0*2) if x1 == oo else x0 + dx/S(2)
-    ym = (1 if y0 == 0 else y0*2) if y1 == oo else y0 + dy/S(2)
-    zm = (1 if z0 == 0 else z0*2) if z1 == oo else z0 + dz/S(2)
-    n = negative3(f, xm, x1, ym, y1, zm, z1)
-    if n != '':
-        return n
-    n = negative3(f, x0, xm, ym, y1, zm, z1)
-    if n != '':
-        return n
-    n = negative3(f, xm, x1, y0, ym, zm, z1)
-    if n != '':
-        return n
-    n = negative3(f, x0, xm, y0, ym, zm, z1)
-    if n != '':
-        return n
-    n = negative3(f, xm, x1, ym, y1, z0, zm)
-    if n != '':
-        return n
-    n = negative3(f, x0, xm, ym, y1, z0, zm)
-    if n != '':
-        return n
-    n = negative3(f, xm, x1, y0, ym, z0, zm)
-    if n != '':
-        return n
-    return negative3(f, x0, xm, y0, ym, z0, zm)
+# return '' if f >= 0, or the counterexample
+# param f: 2-var function about x and y
+def negative_oo(f):
+    x, y = symbols('x, y', negative = False)
+    n = negative(f, 0, 1, 0, 1)
+    if n != None:
+        x0, y0, f0 = n
+        return 'f({},{})={}'.format(x0, y0, f0)
+    n = negative(f.subs(x, 1/x), 0, 1, 0, 1)
+    if n != None:
+        x0, y0, f0 = n
+        return 'f({},{})={}'.format(1/x0, y0, f0)
+    n = negative(f.subs(y, 1/y), 0, 1, 0, 1)
+    if n != None:
+        x0, y0, f0 = n
+        return 'f({},{})={}'.format(y0, 1/y0, f0)
+    n = negative(f.subs(x, 1/x).subs(y, 1/y), 0, 1, 0, 1)
+    if n != None:
+        x0, y0, f0 = n
+        return 'f({},{})={}'.format(1/x0, 1/y0, f0)
+    return ''
 
 def main():
     logging.basicConfig(level = 'INFO')
@@ -140,11 +132,7 @@ def main():
     f = 90*u**7 + 190*u**6*v + 396*u**6 + 120*u**5*v**2 + 542*u**5*v + 630*u**5 + 55*u**4*v**3 + 41*u**4*v**2 + 164*u**4*v + 504*u**4 + 60*u**3*v**4 - 40*u**3*v**3 - 824*u**3*v**2 - 416*u**3*v + 252*u**3 + 25*u**2*v**5 + 115*u**2*v**4 - 332*u**2*v**3 - 984*u**2*v**2 - 156*u**2*v + 72*u**2 + 50*u*v**5 + 206*u*v**4 - 64*u*v**3 - 192*u*v**2 + 72*u*v + 90*v**5 + 216*v**4 + 108*v**3 + 72*v**2
     # intermediate step for sum_cyc(x**3/(8*x**2 + 3*y**2)) - (x + y + z)/11
     # f = 33*u**7 + 69*u**6*v + 143*u**6 + 42*u**5*v**2 + 190*u**5*v + 220*u**5 + 18*u**4*v**3 + 2*u**4*v**2 + 30*u**4*v + 165*u**4 + 21*u**3*v**4 - 24*u**3*v**3 - 336*u**3*v**2 - 194*u**3*v + 77*u**3 + 9*u**2*v**5 + 39*u**2*v**4 - 141*u**2*v**3 - 401*u**2*v**2 - 81*u**2*v + 22*u**2 + 18*u*v**5 + 71*u*v**4 - 42*u*v**3 - 92*u*v**2 + 22*u*v + 33*v**5 + 77*v**4 + 33*v**3 + 22*v**2
-    print('[' + negative(f) + ']')
-    z = symbols('z', negative = False)
-    # https://math.stackexchange.com/q/4765187
-    f = ((1 + x)*(1 + y)*(1 + z))**7 - 7**7*x**4*y**4*z**4
-    print('[' + negative3(f) + ']')
+    print('[' + negative_oo(f) + ']')
 
 if __name__ == '__main__':
     main()
