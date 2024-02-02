e26312890984e9ba398ed46ce1075521e674e1d9
auntyellow <webmaster@xqbase.com>
2024-02-02 16:44:21 +0800

    double upper until trivially non-negative

--- prover/src/main/java/com/xqbase/math/inequality/BinarySearch.java
+++ prover/src/main/java/com/xqbase/math/inequality/BinarySearch.java
@@ -1,7 +1,6 @@
 package com.xqbase.math.inequality;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
 
@@ -17,11 +16,22 @@ public class BinarySearch {
 
 	private static final Rational _0 = Rational.valueOf(0);
 	private static final Rational _1 = Rational.valueOf(1);
-	private static final Rational HALF = new Rational(BigInteger.valueOf(1), BigInteger.valueOf(2));
+	private static final Rational _2 = Rational.valueOf(2);
+	private static final Rational HALF = _1.div(_2);
+	private static final Rational MAX_UPPER = new Rational(BigInteger.ONE.shiftLeft(100));
 	private static final Rational[] EMPTY_RESULT = new Rational[0];
 
 	private static Rational __() {
 		return new Rational(BigInteger.ZERO);
+	}
+
+	private static boolean nonNegative(RationalPoly f) {
+		for (Rational v : f.values()) {
+			if (v.signum() < 0) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	/**
@@ -32,6 +42,9 @@ public class BinarySearch {
 	 * @return [x_i, f(x_i)] if negative found, or [] if f is proved non-negative for all x_i >= 0
 	 */
 	public static Rational[] binarySearch(RationalPoly f) {
+		if (nonNegative(f)) {
+			return EMPTY_RESULT;
+		}
 		String vars = null;
 		for (Mono m : f.keySet()) {
 			vars = m.getVars();
@@ -40,39 +53,21 @@ public class BinarySearch {
 		if (vars == null) {
 			return EMPTY_RESULT;
 		}
-
 		int len = vars.length();
-		int[] degrees = new int[len];
-		Arrays.fill(degrees, 0);
-		for (Mono m : f.keySet()) {
-			short[] exps = m.getExps();
-			for (int i = 0; i < len; i ++) {
-				if (exps[i] > degrees[i]) {
-					degrees[i] = exps[i];
-				}
-			}
-		}
 
 		// m0 helps to find f0 (constant) directly
 		short[] exps0 = new short[len];
 		Arrays.fill(exps0, (short) 0);
 		Mono m0 = new Mono(vars, exps0);
+		Mono[] ms = new Mono[len];
 		// subs helps to generate new f
 		RationalPoly[] subsLower = new RationalPoly[len];
 		RationalPoly[] subsUpper = new RationalPoly[len];
-		// fs and inverses: only one original f and no inverse
-		ArrayList<RationalPoly> fs = new ArrayList<>();
-		fs.add(f);
-		ArrayList<boolean[]> inverses = new ArrayList<>();
-		boolean[] inverse0 = new boolean[len];
-		Arrays.fill(inverse0, false);
-		inverses.add(inverse0);
-		// after each iteration, one var is inverse, fs and inverses doubled
 		for (int i = 0; i < len; i ++) {
-			// prepare subs {{
-			short[] exps_ = exps0.clone();
-			exps_[i] = 1;
-			Mono m = new Mono(vars, exps_);
+			short[] exps = exps0.clone();
+			exps[i] = 1;
+			Mono m = new Mono(vars, exps);
+			ms[i] = m;
 			RationalPoly subs = new RationalPoly();
 			subs.put(m, HALF);
 			subsLower[i] = subs;
@@ -80,48 +75,41 @@ public class BinarySearch {
 			subs.put(m, HALF);
 			subs.put(m0, HALF);
 			subsUpper[i] = subs;
-			// }}
-			RationalPoly[] fs_ = new RationalPoly[fs.size()];
-			fs.toArray(fs_);
-			for (int j = 0; j < fs_.length; j ++) {
-				// fj1 = fj.subs(vi, 1/vi)
-				RationalPoly fj = fs_[j];
-				RationalPoly fj1 = new RationalPoly();
-				for (Map.Entry<Mono, Rational> entry : fj.entrySet()) {
-					short[] exps = entry.getKey().getExps();
-					short[] exps1 = exps.clone();
-					exps1[i] = (short) (degrees[i] - exps[i]);
-					fj1.put(new Mono(vars, exps1), entry.getValue());
-				}
-				fs.add(fj1);
-				boolean[] inverse = inverses.get(j).clone();
-				inverse[i] = true;
-				inverses.add(inverse);
+		}
+
+		// f1 = f.subs(vi, vi + upper), double upper until f1 trivially non-negative
+		Rational upper = _1;
+		while (true) {
+			RationalPoly f1 = f;
+			for (int i = 0; i < len; i ++) {
+				RationalPoly sub = new RationalPoly();
+				sub.put(ms[i], _1);
+				sub.put(m0, upper);
+				f1 = f1.subs(vars.charAt(i), sub);
 			}
+			if (nonNegative(f1)) {
+				break;
+			}
+			if (upper.equals(MAX_UPPER)) {
+				throw new ArithmeticException("Polynomial may be negative for large variables: " + f);
+			}
+			Rational newUpper = __();
+			newUpper.addMul(_2, upper);
+			upper = newUpper;
 		}
 
 		Rational[] coords = new Rational[len];
 		Arrays.fill(coords, _0);
 		Rational[] lengths = new Rational[len];
-		Arrays.fill(lengths, _1);
-
-		int lenFs = fs.size();
-		for (int i = 0; i < lenFs; i ++) {
-			RationalPoly fi = fs.get(i);
-			log.info("Searching " + fi + " ...");
-			Rational[] result = binarySearch(fi, vars, m0, coords, lengths, subsLower, subsUpper);
-			if (result.length == 0) {
-				continue;
-			}
-			boolean[] inverse = inverses.get(i);
-			for (int j = 0; j < len; j ++) {
-				if (inverse[j]) {
-					result[j] = _1.div(result[j]);
-				}
-			}
-			return result;
+		Arrays.fill(lengths, upper);
+		RationalPoly f1 = f;
+		upper = _1.div(upper);
+		for (int i = 0; i < len; i ++) {
+			RationalPoly sub = new RationalPoly();
+			sub.put(ms[i], upper);
+			f1 = f1.subs(vars.charAt(i), sub);
 		}
-		return EMPTY_RESULT;
+		return binarySearch(f1, vars, m0, coords, lengths, subsLower, subsUpper);
 	}
 
 	private static Rational[] binarySearch(RationalPoly f, String vars, Mono m0,
