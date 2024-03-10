232cc2d2084cafaece7cc85e209b8cc7a85232d3
auntyellow <webmaster@xqbase.com>
2024-03-10 22:45:03 +0800

    inversion-1: try eliminate G and H first

--- pythagoras/ritt-wu/inversion-1u.py
+++ pythagoras/ritt-wu/inversion-1u.py
@@ -13,7 +13,7 @@ def det(row_def, *points):
         mat.append(row_def(point))
     return Matrix(mat).det()
 
-def dist(P1, P2, d2):
+def dist2(P1, P2, d2):
     return (P1[0] - P2[0])**2 + (P1[1] - P2[1])**2 - d2
 
 def main():
@@ -23,23 +23,27 @@ def main():
     # xd, yd, xg, yg, xh, yh are undetermined
     A, B, C, E, F = (-(a + 1)*e, (a + 1)*f), (0, 0), ((c + 1)*e, (c + 1)*f), (-e, f), (e, f)
     D, G, H = (xd, yd), (xg, yg), (xh, yh)
-    print('BE = BF?', dist(B, E, dist(B, F, 0)) == 0)
+    print('BE = BF?', dist2(B, E, dist2(B, F, 0)) == 0)
     print('Is AEB collinear?', det(collinear, A, E, B) == 0)
     print('Is BFC collinear?', det(collinear, B, F, C) == 0)
-    f1 = det(collinear, C, G, D)   # G, D
-    # CG = CF
-    f2 = dist(C, G, dist(C, F, 0)) # G
-    f3 = dist(G, D, d**2)          # G, D
-    f4 = det(collinear, A, H, D)   # H, D
-    # AE = AH
-    f5 = dist(A, H, dist(A, E, 0)) # H
-    f6 = dist(H, D, d**2)          # H, D
-    g = det(concyclic, E, F, G, H) # G, H
+    h1 = det(collinear, C, G, D)     # G, D
+    h2 = dist2(C, G, dist2(C, F, 0)) # G, CG = CF
+    h3 = dist2(G, D, d**2)           # G, D
+    h4 = det(collinear, A, H, D)     # H, D
+    h5 = dist2(A, H, dist2(A, E, 0)) # H, AE = AH
+    h6 = dist2(H, D, d**2)           # H, D
+    g = det(concyclic, E, F, G, H)   # G, H
+    B1 = groebner([h1, h2, h3], xg, yg, xd, yd)
+    h7 = B1[len(B1) - 1]
+    print('h7 =', h7)
+    B2 = groebner([h4, h5, h6], xh, yh, xd, yd)
+    h8 = B2[len(B2) - 1]
+    print('h8 =', h8)
     # too slow
-    '''
-    B = groebner([f1, f2, f3, f4, f5, f6], xd, yd, xg, yg, xh, yh)
+    # '''
+    B = groebner([h7, h8], xd, yd)
     print(B, len(B))
-    '''
+    # '''
 
 if __name__ == '__main__':
     main()
